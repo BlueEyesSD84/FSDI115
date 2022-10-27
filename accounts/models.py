a76ae6c2aad85e9508e74b5908c491225d0dc602
Jimmy Smith <jimmy.a.smith@gmail.com>
2022-10-27 15:32:28 -0700

    Add files via upload

---
+++ accounts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(blank=True, default='', unique=True)
+    first_name = models.CharField(max_length=255, blank=True, default='')
+    last_name = models.CharField(max_length=255, blank=True, default='')
+    username = models.CharField(max_length=255, blank=True, default='', unique=True)
